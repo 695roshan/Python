b18875305abbf13aeb4de9ab634154d7cb9f503a
Roshan Roy <79837320+695roshan@users.noreply.github.com>
2021-07-15 11:16:12 +0530

    Add files via upload

---
+++ Extension.py
@@ -0,0 +1,21 @@
+filename=input("Input the filename: ")
+ex=filename[filename.index(".")+1:]
+if (ex=="py"):
+    print("The extension of the file is : 'python'")
+elif(ex=="c"):
+    print("The extension of the file is : 'C'")
+elif(ex=="cpp"):
+    print("The extension of the file is : 'C++'")
+elif(ex=="java"):
+    print("The extension of the file is : 'Java'")
+elif(ex=="txt"):
+    print("The extension of the file is : 'Text file'")
+elif(ex=="pdf"):
+    print("The extension of the file is : 'PDF'")
+elif(ex=="docx"):
+    print("The extension of the file is : 'Document'")
+elif(ex=="exe"):
+    print("The extension of the file is : 'Executable file'")
+
+    
+    
