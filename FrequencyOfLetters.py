0e6f91584af4d4338d08912988284781352ff2fd
Roshan Roy <79837320+695roshan@users.noreply.github.com>
2021-07-16 13:40:36 +0530

    Add files via upload

---
+++ FrequencyOfLetters.py
@@ -0,0 +1,17 @@
+dict={}
+list=[]
+def most_frequent(st):
+    for i in st:   
+        dict[i]=st.count(i)
+    for j in dict:
+        list.append(dict[j])
+s=input("Please enter a string: ")
+mostFreq=most_frequent(s)
+list.sort(reverse=True)
+for i in list:
+    for key,value in dict.items():
+        if value == i:
+            print(key,"=",value, end=" ")
+            dict[key]=0
+            break
+        
